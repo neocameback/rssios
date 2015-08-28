9d9cad3af81349b0cf74569ca332e5d8f4b080a4
Huy Nguyen <jetstar89@gmail.com>
2015-08-28 10:19:35 +0700

    add afgziprequest

---
+++ MyRssReader/Tab home/FileListViewController.h
@@ -0,0 +1,19 @@
+//
+//  FileListViewController.h
+//  MyRssReader
+//
+//  Created by Huyns89 on 8/28/15.
+//  Copyright (c) 2015 Huyns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface FileListViewController : BaseViewController <UITableViewDataSource, UITableViewDelegate, GADInterstitialDelegate>
+{
+    __weak IBOutlet UITableView *_tableView;
+    NSMutableArray *nodeList;
+    NSArray *searchResults;
+    GADInterstitial *interstitial_;
+}
+@end
---
+++ MyRssReader/Tab home/FileListViewController.m
@@ -0,0 +1,282 @@
+//
+//  FileListViewController.m
+//  MyRssReader
+//
+//  Created by Huyns89 on 8/28/15.
+//  Copyright (c) 2015 Huyns. All rights reserved.
+//
+
+#import "FileListViewController.h"
+#import "TempNode.h"
+#import "NodeListCustomCell.h"
+#import "RPNodeDescriptionViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import "NodeListViewController.h"
+#import <DMPlayerViewController.h>
+#import "WebViewViewController.h"
+#import <XCDYouTubeKit.h>
+
+@interface FileListViewController ()
+{
+    MPMoviePlayerViewController *moviePlayer;
+    NodeListCustomCell *nodeCell;
+    NSString *identifier;
+    TempNode *currentNode;
+}
+@end
+
+@implementation FileListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    identifier = @"NodeListCustomCell";
+    UINib *nib = [UINib nibWithNibName:identifier bundle:nil];
+    nodeCell = [nib instantiateWithOwner:self options:nil][0];
+    [_tableView registerNib:nib forCellReuseIdentifier:identifier];
+    [self.searchDisplayController.searchResultsTableView registerNib:nib forCellReuseIdentifier:identifier];
+    
+    interstitial_ = [self createAndLoadInterstital];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+        TempNode *node = nodeList[indexPath.row];
+        [nodeCell configWithNode:node];
+        [nodeCell layoutIfNeeded];
+        CGFloat height = [nodeCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+        if (height < 71) {
+            return 71;
+        }else{
+            return height + 1;
+        }
+}
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return [searchResults count];
+    } else {
+        return nodeList.count;
+    }
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    TempNode *node = nil;
+    
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        node = searchResults[indexPath.row];
+    } else {
+        node = nodeList[indexPath.row];
+    }
+    
+    NodeListCustomCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
+    [cell configWithNode:node];
+    
+    return cell;
+}
+
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        
+    currentNode = nil;
+    
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        currentNode = searchResults[indexPath.row];
+    } else {
+        currentNode = nodeList[indexPath.row];
+    }
+    
+    [self continueAtCurrentPath];
+    return;
+    switch ([Common typeOfNode:currentNode.nodeType]) {
+        case NODE_TYPE_RSS:
+        {
+            /**
+             *  parse rss/xml
+             */
+            [self continueAtCurrentPath];
+        }
+            break;
+        case NODE_TYPE_VIDEO:
+        {
+            [self preLoadInterstitial];
+        }
+            break;
+        case NODE_TYPE_MP4:
+        {
+            [self preLoadInterstitial];
+        }
+            break;
+        case NODE_TYPE_YOUTUBE:
+        {
+            [self preLoadInterstitial];
+        }
+            break;
+        case NODE_TYPE_DAILYMOTION:
+        {
+            [self preLoadInterstitial];
+        }
+            break;
+        case NODE_TYPE_RTMP:
+        {
+            [self preLoadInterstitial];
+        }
+            break;
+            
+        default:
+        {
+            [self preLoadInterstitial];
+        }
+            break;
+    }
+}
+#pragma mark Admob
+#pragma mark Interstitial delegate
+-(GADInterstitial*) createAndLoadInterstital
+{
+    GADRequest *request = [GADRequest request];
+    GADInterstitial * interstitial = nil;
+        interstitial = [[GADInterstitial alloc] initWithAdUnitID:kLargeAdUnitId];
+    interstitial.delegate = self;
+    [interstitial loadRequest:request];
+    
+    return interstitial;
+}
+
+- (void)preLoadInterstitial {
+    //Call this method as soon as you can - loadRequest will run in the background and your interstitial will be ready when you need to show it
+    
+    NSDate *lastOpenDate = [[NSUserDefaults standardUserDefaults] objectForKey:kLastOpenFullScreen];
+    NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:lastOpenDate];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:[NSDate date] forKey:kLastOpenFullScreen];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    if (interval <= kSecondsToPresentInterstitial) {
+        [self continueAtCurrentPath];
+        return;
+    }
+    if (interstitial_.isReady) {
+        [interstitial_ presentFromRootViewController:self];
+    }else{
+        [self continueAtCurrentPath];
+    }
+}
+
+- (void)interstitialDidReceiveAd:(GADInterstitial *)interstitial
+{
+    NSLog(@"interstitialDidReceiveAd");
+}
+- (void)interstitial:(GADInterstitial *)interstitial didFailToReceiveAdWithError:(GADRequestError *)error
+{
+    NSLog(@"didFailToReceiveAdWithError: %@",[error localizedDescription]);
+    //If an error occurs and the interstitial is not received you might want to retry automatically after a certain interval
+    [self createAndLoadInterstital];
+}
+
+- (void)interstitialDidDismissScreen:(GADInterstitial *)interstitial
+{
+    [self createAndLoadInterstital];
+    [self continueAtCurrentPath];
+}
+
+-(void) continueAtCurrentPath
+{
+    /**
+     *  check if node url is empty or not
+     */
+    if (!currentNode.nodeUrl || currentNode.nodeUrl.length <= 0) {
+        
+        RPNodeDescriptionViewController *viewcontroller = [Storyboard instantiateViewControllerWithIdentifier:@"RPNodeDescriptionViewController"];
+        [viewcontroller setTitle:currentNode.nodeTitle];
+        [viewcontroller setDesc:currentNode.nodeDesc];
+        [viewcontroller setUrl:currentNode.nodeLink];
+        [self.navigationController pushViewController:viewcontroller animated:YES];
+        return;
+    }
+    /**
+     *  otherwise
+     */
+    switch ([Common typeOfNode:currentNode.nodeType]) {
+        case NODE_TYPE_RSS:
+        {
+            NodeListViewController *viewcontroller = [NodeListViewController initWithNibName];
+            [viewcontroller setRssLink:currentNode.nodeUrl];
+            [viewcontroller setTitle:currentNode.nodeTitle];
+            [self.navigationController pushViewController:viewcontroller animated:YES];
+        }
+            break;
+        case NODE_TYPE_VIDEO:
+        {
+            moviePlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:currentNode.nodeUrl]];
+            [self presentMoviePlayerViewControllerAnimated:moviePlayer];
+        }
+            break;
+        case NODE_TYPE_MP4:
+        {
+            moviePlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:currentNode.nodeUrl]];
+            [self presentMoviePlayerViewControllerAnimated:moviePlayer];
+        }
+            break;
+        case NODE_TYPE_YOUTUBE:
+        {
+            XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:[currentNode.nodeUrl extractYoutubeId]];
+            [self presentMoviePlayerViewControllerAnimated:videoPlayerViewController];
+        }
+            break;
+        case NODE_TYPE_DAILYMOTION:
+        {
+            NSString *url = currentNode.nodeUrl;
+            NSString *videoIdentifer = [url lastPathComponent];
+            
+            DMPlayerViewController *playerViewcontroller = [[DMPlayerViewController alloc] initWithVideo:videoIdentifer];
+            [playerViewcontroller setTitle:currentNode.nodeTitle];
+            [playerViewcontroller setHidesBottomBarWhenPushed:YES];
+            [self.navigationController pushViewController:playerViewcontroller animated:YES];
+        }
+            break;
+        case NODE_TYPE_RTMP:
+        {
+        }
+            break;
+        case NODE_TYPE_WEB_CONTENT:
+        {
+            
+        }
+            break;
+        default:
+        {
+            WebViewViewController *viewcontroller = [WebViewViewController initWithNibName];
+            [viewcontroller setTitle:currentNode.nodeTitle];
+            [viewcontroller setWebUrl:currentNode.nodeUrl];
+            [self.navigationController pushViewController:viewcontroller animated:YES];
+        }
+            break;
+    }
+}
+
+#pragma mark search bar implement
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"nodeTitle contains[c] %@", searchText];
+    searchResults = [nodeList filteredArrayUsingPredicate:resultPredicate];
+}
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString
+                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
+                                      objectAtIndex:[self.searchDisplayController.searchBar
+                                                     selectedScopeButtonIndex]]];
+    
+    return YES;
+}
+
+@end
